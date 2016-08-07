e694717ac1eceacf9d641b356e763f4a82de29f1
Sebastien Melki <sebastienmelki@gmail.com>
2016-08-07 14:32:03 +0300

    Initial react-native project created

---
+++ GoLocl/index.ios.js
@@ -0,0 +1,47 @@
+/**
+ * @author Sebastien Melki <sebastienmelki@gmail.com>
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class GoLocl extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to GoLocl!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, please create an account
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('GoLocl', () => GoLocl);
